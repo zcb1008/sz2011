3efabbf3be968a2b4d954061933b7083e2d3b7bd
ZCBdebug <44228643+zcb1008@users.noreply.github.com>
2020-12-18 16:39:39 +0800

    Update and rename php/common.php to 的PHP/common.php

--- 的PHP/common.php
+++ 的PHP/common.php
@@ -1,6 +1,6 @@
 <?php
 header("content-type:text/html;charset=utf-8");
 //连接数据库
-$link=mysqli_connect('localhost:3308','root','','db_tcl');
+$link=mysqli_connect('localhost:3306','root','','db_tcl');
 mysqli_set_charset($link,"utf8");
 ?>
